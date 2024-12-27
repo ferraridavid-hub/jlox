1c3ba2dc10f1c61605127f0654c6258ca4f4e187
david.ferrari <david.ferrari@primeurad.com>
2024-12-27 01:34:57 +0100

    Working version of scanner

--- src/main/java/com/primeur/Scanner.java
+++ src/main/java/com/primeur/Scanner.java
@@ -1,7 +1,9 @@
 package com.primeur;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class Scanner {
     private final String source;
@@ -9,6 +11,28 @@ public class Scanner {
     private int start = 0;
     private int current = 0;
     private int line = 1;
+
+    private static final Map<String, TokenType> keywords;
+
+    static {
+        keywords = new HashMap<>();
+        keywords.put("and", TokenType.AND);
+        keywords.put("class", TokenType.CLASS);
+        keywords.put("else", TokenType.ELSE);
+        keywords.put("false", TokenType.FALSE);
+        keywords.put("for", TokenType.FOR);
+        keywords.put("fun", TokenType.FUN);
+        keywords.put("if", TokenType.IF);
+        keywords.put("nil", TokenType.NIL);
+        keywords.put("or", TokenType.OR);
+        keywords.put("print", TokenType.PRINT);
+        keywords.put("return", TokenType.RETURN);
+        keywords.put("super", TokenType.SUPER);
+        keywords.put("this", TokenType.THIS);
+        keywords.put("true", TokenType.TRUE);
+        keywords.put("var", TokenType.VAR);
+        keywords.put("while", TokenType.WHILE);
+    }
 
     Scanner(String source) {
         this.source = source;
@@ -104,13 +128,40 @@ public class Scanner {
                 number();
                 break;
             default:
-                Lox.error(line, "Unexpected character.");
+                if (isAlpha(c)) {
+                    identifier();
+                } else {
+                    Lox.error(line, "Unexpected character.");
+                }
                 break;
         }
     }
 
+    private void identifier() {
+        while(isAlphaNumeric(peek())) {
+            advance();
+        }
+
+        String text = source.substring(start, current);
+        TokenType type = keywords.get(text);
+        if (type == null) {
+            type = TokenType.IDENTIFIER;
+        }
+        addToken(type);
+    }
+
+    private boolean isAlphaNumeric(char c) {
+        return isDigit(c) || isAlpha(c);
+    }
+
+    private boolean isAlpha(char c) {
+        return (c >= 'a' && c <= 'z') ||
+                (c >= 'A' && c <= 'Z') ||
+                (c == '_');
+    }
+
     private void number() {
-        while(isDigit(peek())) {
+        while (isDigit(peek())) {
             advance();
         }
 
@@ -129,7 +180,7 @@ public class Scanner {
     }
 
     private void string() {
-        while(!isAtEnd() && peek() != '"') {
+        while (!isAtEnd() && peek() != '"') {
             if (peek() == '\n') {
                 line++;
             }
